15a75b099f7376c693228565efd2c126fbdf1140
luissacouto <luis.sa.couto@tecnico.ulisboa.pt>
2024-09-13 19:09:34 +0100

    rollback

--- app/main.py
+++ app/main.py
@@ -14,9 +14,10 @@ app = FastAPI(
 def hello_world():
     # engine = create_async_engine(DATABASE_URL, echo=True)
     # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
-    DB_HOST = os.getenv("DB_HOST")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
-    beautiful = f"\n\n\nHOST: {DB_HOST}\n\n\USER: {DB_USER}\n\n\PASS: {DB_PASSWORD}\n\n\NAME: {DB_NAME}\n\n\n"
-    return {"Hello": f"World: {beautiful}"}
+    # DB_HOST = os.getenv("DB_HOST")
+    # DB_USER = os.getenv("DB_USER")
+    # DB_PASSWORD = os.getenv("DB_PASSWORD")
+    # DB_NAME = os.getenv("DB_NAME")
+    # beautiful = f"\n\n\nHOST: {DB_HOST}\n\n\USER: {DB_USER}\n\n\PASS: {DB_PASSWORD}\n\n\NAME: {DB_NAME}\n\n\n"
+    # return {"Hello": f"World: {beautiful}"}
+    return {"Hello": "World beautiful"}
